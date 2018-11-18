ac7dab5dad05913f87a5a5705f315288bac1449c
Kim Cyriel S. Avillanosa <ksavillanosa.itd.pps@gmail.com>
2018-11-18 17:25:32 +0800

    Added services methods on accounts

---
+++ MiniClinic/Clinic.API/Controllers/AccountsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Accounts")]
+    public class AccountsController : Controller
+    {
+        public AccountsController()
+        {
+
+        }
+
+        public async Task<IActionResult> Login(string Username,string Password)
+        {
+            try
+            {
+                return Ok(true);
+            }
+            catch
+            {
+                return BadRequest(false);
+            }
+        }
+    }
+}
---
+++ MiniClinic/Clinic.Services/Abstractions/Service/IAccountsService.cs
@@ -0,0 +1,16 @@
+﻿using Clinic.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Services.Abstractions.Service
+{
+    public interface IAccountsService
+    {
+        Task<(Account, bool)> Login(string Username, string Password);
+        Task<bool> CreateAccount(Account account);
+        Task<bool> ModifyAccount(Account account);
+        Task<bool> RemoveAccount(Account account);
+    }
+}
